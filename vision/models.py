cbb2841944692b171d3b19fd937c213043ba0f59
ivy <intotheskywithivy@gmail.com>
2018-09-28 11:26:37 +1000

    Added "post" model and migrated the changes

--- vision/models.py
+++ vision/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    post_title = models.CharField(max_length=255, null=True, blank=True)
+    post_body = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.post_title
