c9a27c8c03c8915ed6025fc797c33ab2c4b15e39
ivy <intotheskywithivy@gmail.com>
2018-09-28 17:03:58 +1000

    added post_detail pages

--- vision/urls.py
+++ vision/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'vision'
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:post_id>/post_detail", views.post_detail, name="post_detail")
 ]
--- vision/views.py
+++ vision/views.py
@@ -4,3 +4,7 @@ from .models import Post
 def index(request):
     post_filter = Post.objects.all().order_by("-post_time")
     return render(request, "vision/index.html", {"post_filter":post_filter})
+
+def post_detail(request, post_id):
+    post_filter = Post.objects.filter(post_id=post_id)
+    return render(request, 'vision/post_detail.html', {"post_filter":post_filter})
