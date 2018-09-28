9040072d3bd8ee2b0b6584afa046e8dc6958bf86
ivy <intotheskywithivy@gmail.com>
2018-09-28 11:09:49 +1000

    maybe fixed urls

--- social_network/urls.py
+++ social_network/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('vision.urls')),
+    path('vision/', include('vision.urls')),
     path('admin/', admin.site.urls),
 ]
