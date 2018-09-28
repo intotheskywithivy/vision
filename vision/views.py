06d415337a5c61230fdb0c64c53477fbd1e37fbe
ivy <intotheskywithivy@gmail.com>
2018-09-28 16:10:05 +1000

    yeet

--- vision/views.py
+++ vision/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
-    post_filter = Post.objects.all().order_by("post_time")
+    post_filter = Post.objects.all().order_by("-post_time")
     return render(request, "vision/index.html", {"post_filter":post_filter})
